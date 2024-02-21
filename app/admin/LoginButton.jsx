803780a2b23ab40e77d03e6752446cb89ada1fd9
registros <89086611+acavih@users.noreply.github.com>
2024-02-21 23:16:51 +0000

    Form login

--- app/admin/LoginButton.jsx
+++ app/admin/LoginButton.jsx
@@ -2,12 +2,33 @@
 
 import { Button } from "@mantine/core"
 import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function LoginButton(props) {
+    const router = useRouter()
     console.log(props)
     return (
-        <Button onClick={() => signIn()}>
-            Login
-        </Button>
+        <div>
+            <Button onClick={() => signIn()}>
+                Login
+            </Button>
+            <form onSubmit={async (e) => {
+                e.preventDefault()
+                const username = e.target['username'].value
+                const password = e.target['password'].value
+
+                const result = await signIn('credentials', {
+                    redirect: false, username, password
+                })
+
+                console.log(result)
+                if (result.ok)
+                    router.refresh()
+            }}>
+                <input type="text" name="username" placeholder="email" />
+                <input type="password" name="password" placeholder="password" />
+                <input type="submit" value={'login'} />
+            </form>
+        </div>
     )
 }
